d46909f12e8f02b6ee862a4149cf0c23ef6a733b
Nex <nex@nex.sx>
2018-01-13 13:10:20 +0100

    Storing full information on processes

--- processes.go
+++ processes.go
@@ -8,27 +8,47 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
+type RunningProcess struct {
+	Pid int32 `json:"pid"`
+	Name string `json:"name"`
+	ParentPid int32 `json:"ppid"`
+	Exe string `json:"exe"`
+	Cmdline string `json:"cmd"`
+}
+
 func generateProcessList() {
 	log.Info("Generating list of running processes...")
 
-	processes, err := process.Processes()
+	procs, err := process.Processes()
 	if err != nil {
 		log.Error("Unable to create process list: ", err.Error())
 		return
 	}
 
-	processesJsonPath := filepath.Join(acq.Storage, "processes.json")
-	processesJson, err := os.Create(processesJsonPath)
+	var processList []*RunningProcess
+	for _, proc := range procs {
+		entry := RunningProcess{
+			Pid: proc.Pid(),
+			Name: proc.Name(),
+			ParentPid: proc.Ppid(),
+			Exe: proc.Exe(),
+			Cmdline: proc.Cmdline()
+		}
+		processList = append(processList, entry)
+	}
+
+	processListPath := filepath.Join(acq.Storage, "processlist.json")
+	processListJson, err := os.Create(processListPath)
 	if err != nil {
 		log.Error("Unable to save process list to file: ", err.Error())
 		return
 	}
-	defer processesJson.Close()
+	defer processListJson.Close()
 
-	buf, _ := json.MarshalIndent(processes, "", "    ")
+	buf, _ := json.MarshalIndent(processList, "", "    ")
 
-	processesJson.WriteString(string(buf[:]))
-	processesJson.Sync()
+	processListJson.WriteString(string(buf[:]))
+	processListJson.Sync()
 
 	log.Info("Process list generated successfully!")
 }
