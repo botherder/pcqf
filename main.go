74e10cedc874820861478d5a132bda62a5912046
Nex <nex@nex.sx>
2019-07-23 22:48:27 +0200

    Added license

--- main.go
+++ main.go
@@ -1,3 +1,20 @@
+// Copyright (c) 2017-2019 Claudio Guarnieri.
+//
+// This file is part of Snoopdigg.
+//
+// Snoopdigg is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Snoopdigg is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Snoopdigg.  If not, see <https://www.gnu.org/licenses/>.
+
 package main
 
 import (
@@ -21,7 +38,7 @@ func main() {
 	fmt.Println("|___/_| |_|\\___/ \\___/| .__/ \\__,_|_|\\__, |\\__, |")
 	fmt.Println("                      | |             __/ | __/ |     ")
 	fmt.Println("                      |_|            |___/ |___/      ")
-	fmt.Println("    (c) 2017-2018 Claudio Guarnieri (nex@nex.sx)      ")
+	fmt.Println("    (c) 2017-2019 Claudio Guarnieri (nex@nex.sx)      ")
 	fmt.Println("                                                      ")
 
 	// Set up the logging.
@@ -37,14 +54,14 @@ func main() {
 	generateAutoruns()
 
 	virt, _ := mem.VirtualMemory()
-	total := virt.Total / 1000000000
+	total := virt.Total / (1000 * 1000 * 1000)
 
 	log.Warning("Do you want to take a memory snapshot (it will take circa ", total, " GB of space) ? [y/N]")
 
 	reader := bufio.NewReader(os.Stdin)
 	choice, _ := reader.ReadString('\n')
-	choice = strings.TrimSpace(choice)
-	if choice == "y" || choice == "Y" {
+	choice = strings.TrimSpace(strings.ToLower(choice))
+	if choice == "y" {
 		generateMemoryDump()
 	} else {
 		log.Info("Skipping memory acquisition.")
@@ -53,7 +70,6 @@ func main() {
 	storeSecurely()
 
 	log.Info("Acquisition completed.")
-
 	log.Info("Press Enter to finish ...")
 	var b = make([]byte, 1)
 	os.Stdin.Read(b)
